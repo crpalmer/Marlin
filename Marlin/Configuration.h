d77c5b496544684c6395370eb650970926545048
Christopher R. Palmer <crpalmer@gmail.com>
2019-08-07 07:40:57 -0400

    Run pid autotune and also enable pid for the bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -474,14 +474,19 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
   // Creality Ender-5
-  #define DEFAULT_Kp 21.73
-  #define DEFAULT_Ki 1.54
-  #define DEFAULT_Kd 76.55
+  //#define DEFAULT_Kp 21.73
+  //#define DEFAULT_Ki 1.54
+  //#define DEFAULT_Kd 76.55
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
   //#define DEFAULT_Ki 1.08
   //#define DEFAULT_Kd 114
+
+  // Ender 5 pid tuned at 205
+  #define DEFAULT_Kp 20.94
+  #define DEFAULT_Ki 1.59
+  #define DEFAULT_Kd 69.04
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -512,7 +517,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-//#define PIDTEMPBED
+#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -530,9 +535,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  //#define DEFAULT_bedKp 10.00
+  //#define DEFAULT_bedKi .023
+  //#define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
@@ -541,6 +546,12 @@
   //#define DEFAULT_bedKd 1675.16
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
+
+  // Ender 5 pid tuned at 60
+  #define DEFAULT_bedKp 321.60
+  #define DEFAULT_bedKi 63.32
+  #define DEFAULT_bedKd 408.36
+
 #endif // PIDTEMPBED
 
 // @section extruder
