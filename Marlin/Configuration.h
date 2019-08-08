250c4b433421430b914034ff1671b7fa77e52210
Christopher R. Palmer <crpalmer@gmail.com>
2019-08-08 10:46:21 -0400

    Enable junction deviation and s-curve acceleration
    The .08 is derived from:
    
    http://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
    
    which says that the default jerk rate of 10mm/sec^3 and the default
    acceleration of 500mm/sec^2 gives us
    
    .4*10*10/500 = 0.08
    
    This costs 4.6K of program space and 1K of variable space.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -770,9 +770,9 @@
  *   https://reprap.org/forum/read.php?1,739819
  *   http://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
 */
-//#define JUNCTION_DEVIATION
+#define JUNCTION_DEVIATION
 #if ENABLED(JUNCTION_DEVIATION)
-  #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.08  // (mm) Distance from real junction edge
 #endif
 
 /**
@@ -799,7 +799,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-//#define S_CURVE_ACCELERATION
+#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
